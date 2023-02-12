ca2453e7f285e076b0fcf99d44d182bb116d545d
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-12 16:21:06 +0200

    created async/await in useEffect, explanation coms

--- src/Components/PostsHolder/PostsHolder.js
+++ src/Components/PostsHolder/PostsHolder.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 import Post  from '../Post/Post'
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
 function PostsHolder() {
+
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+      const fetchData = async () => {               // Structure of getting array of objects from server and catching mistakes 
+        try {                                       // Using useEffect to avoid loop rerendering 
+            const res = await fetch(API_URL)        // We can't get async/await as a first argument of useEffect cause it returs promise, 
+            const posts = await res.json()          // but the function in first argument of hook useEffect should return undefiend or 
+            setPosts(posts)                         // other function, thats why i assign the async/await to arrow func fetchData 
+        } catch {
+
+        }
+      }
+       fetchData() 
+    
+    }, [])
+    
+
   return (
     <div>
         <Post />
