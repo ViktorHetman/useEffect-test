945d759607a8bd864bc56016012ad8877d61d862
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-12 16:02:27 +0200

    init react app, created components structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PostsHolder from './Components/PostsHolder/PostsHolder'
+
+function App() {
+  return (
+    <div className="App">
+      <PostsHolder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/PostsHolder/PostsHolder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post  from '../Post/Post'
+
+function PostsHolder() {
+  return (
+    <div>
+        <Post />
+    </div>
+  )
+}
+
+export default PostsHolder
